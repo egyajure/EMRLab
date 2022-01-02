90a175ff4d80e19a9ce8ca524331c109e66d6909
Elena Yajure <egyajure@gmail.com>
2022-01-02 15:50:50 -0700

    cleaner version

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main (){
     //section one, taking in voltage and frequency
-    cout<< "started\n";
 
     //initializing dynamic arrays
     vector <double> frequency;
@@ -20,19 +19,14 @@ int main (){
 
     //asking for number of data points
     cin>> numPoints; cin.ignore();
-    cout<< numPoints<<"\n";
-
-    cout<< "test point reached\n";
 
     //looping through number of data points twice, once per type of variable
 
     for (int j = 0; j < numPoints; j++){ 
-        cout<< "first loop started \n";
         cin >> pushBack; cin.ignore();
         frequency.push_back(pushBack);
     }
     for (int j = 0; j < numPoints; j++){ 
-        cout<< "second loop started \n";
         cin >> pushBack; cin.ignore();
         voltage.push_back(pushBack);
     }  
@@ -40,7 +34,6 @@ int main (){
     //checking if there is another input
     string endCheck;
     cin >> endCheck;
-    cout << "endCheck: " << endCheck << "\n";
 
     //explaining how to do an input if there is a mistake
     if (endCheck != "end"){
@@ -52,22 +45,6 @@ int main (){
     }
     //running the rest of the code if there is no mistake
     else {
-        //test for frequency
-        for (int i = 0;i < frequency.size(); i++){
-            cout<< "frequency " << i+1 << ":" << frequency[i] << "\n";
-        }
-
-        //test for voltage
-        for (int i = 0;i < voltage.size(); i++){
-            cout<< "voltage " << i+1 << ":" << voltage[i] << "\n";
-        }
-
-        //test for line of best fit functions
-        //for this experiment frequency is the value on the X axis and voltage is on the y axis
-        cout << "sum x: " << sumX(frequency) << "\n";
-        cout << "sum y: " << sumY(voltage) << "\n";
-        cout << "sum XY: " << sumYX(frequency, voltage) << "\n";
-        cout << "sum x squared: " << sumXsquared(frequency)<< "\n";
         
         //finding slope and y intercept
         double slope = 0;
@@ -75,15 +52,9 @@ int main (){
         slope = (numPoints * sumYX(frequency,voltage) - sumX(frequency) * sumY(voltage)) / (numPoints * sumXsquared(frequency) - sumX(frequency) * sumX(frequency));
         yInt = (sumY(voltage) - slope * sumX(frequency)) / numPoints;
 
-        //test, printing slope and y-int
-        cout << "slope: " << slope << "\n";
-        cout << "y-int: " << yInt << "\n";
-
         //printing out found EMR terms
         cout << "work function: " << workFunction(yInt) << "\n";
         cout << "threshold frequency: " << thresFrequency(slope, yInt) << "\n";
         cout << "Plank's constant: " << constant(slope) << "\n";
-
-
         }
 }
